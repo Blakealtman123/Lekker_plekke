be7bfc053a5a976e9df382563e7f9a0beb5ed83a
blakealtman123 <blakealtman123@gmail.com>
2015-05-26 23:50:49 +0200

    d7 final

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+    Place.create({name: 'Once' , address: '74 Kloof St.'})
+    Place.create({name: 'Aces of Spades', address: 'Blackout Alley'})
+    
